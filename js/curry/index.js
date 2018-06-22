ec421b8c1141e2c2811e644b79278b845d6fa0cd
Vitaly <edguarvit@gmail.com>
2018-06-22 14:07:40 +0300

    curry

--- js/curry/index.js
+++ js/curry/index.js
@@ -6,11 +6,27 @@ function abcdef(a, b, c, d, e, f) {
   return a + b + c + d + e + f;
 }
 
+
+function curry() {
+    var str = this(...arguments);
+
+    if (String(str).indexOf('undefined') === -1) {
+        return str;
+    } else {
+        return curry.bind(this, ...arguments);
+    }
+}
+
 function assert (a, b) {
   if (a !== b) {
     throw new Error();
   }
 }
+
+abc.curry = curry;
+abcdef.curry = curry;
+
+
 assert(abc.curry('A', 'B', 'C'), 'ABC');
 assert(abc.curry('A')('B')('C'), 'ABC');
 assert(abc.curry('A', 'B')('C'), 'ABC');
